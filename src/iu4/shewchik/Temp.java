64e595c10e001a64aa36397dcbc2be4c2379e67a
Shevchik <andrey.shevchik@gmail.com>
2013-09-21 02:14:34 +0400

    Добавление пакетов

---
+++ src/iu4/shewchik/Temp.java
@@ -0,0 +1,11 @@
+package iu4.shewchik;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Shevchik
+ * Date: 21.09.13
+ * Time: 2:02
+ * To change this template use File | Settings | File Templates.
+ */
+public class Temp {
+}
