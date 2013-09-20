20cec23be8469c5039454ffe2db42d65eedb72a3
Maks12121 <maks12121@rambler.ru>
2013-09-20 23:22:17 +0400

    Создал класс

---
+++ src/iu4/chernuhin/TestIntOut.java
@@ -0,0 +1,11 @@
+package iu4.chernuhin;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Макс
+ * Date: 20.09.13
+ * Time: 23:21
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestIntOut {
+}
