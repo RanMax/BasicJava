e8a312055c925a253ff7077c873de18911627229
Eddys0n <isaev.ed@gmail.com>
2013-09-21 02:58:32 +0400

    Добавление пакетов

---
+++ src/iu4/isaev/TestClass666.java
@@ -0,0 +1,15 @@
+package iu4.isaev;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Eduard.Isaev
+ * Date: 21.09.13
+ * Time: 2:56
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestClass666 {
+
+    public static void main(String[] args) {
+        System.exit(1);
+    }
+}
