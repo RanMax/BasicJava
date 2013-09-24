59c44d03f3ab23d078a642a53122f14fe4b6f211
Maxim Chernov <ChernovMaxim@mail.ru>
2013-09-24 07:56:00 +0400

    Добавленеи пакетов для группы РТ4

---
+++ src/rt4/chistov/TestClass.java
@@ -0,0 +1,11 @@
+package rt4.chistov;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Maxim
+ * Date: 24.09.13
+ * Time: 7:54
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestClass {
+}
---
+++ src/rt4/samoilov/TestClass.java
@@ -0,0 +1,11 @@
+package rt4.samoilov;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Maxim
+ * Date: 24.09.13
+ * Time: 7:55
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestClass {
+}
