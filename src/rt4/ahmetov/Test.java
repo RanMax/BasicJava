1dfcb51028da7543cf62eec10e84617c8cec7353
Maxim Chernov <ChernovMaxim@mail.ru>
2013-09-27 00:13:09 +0400

    Добавление пакетов

--- src/rt4/ahmetov/Test.java
+++ src/rt4/ahmetov/Test.java
@@ -1,11 +1,9 @@
 package rt4.ahmetov;
 
 /**
- * Created with IntelliJ IDEA.
  * User: Maxim
  * Date: 27.09.13
  * Time: 0:11
- * To change this template use File | Settings | File Templates.
  */
 public class Test {
 }
