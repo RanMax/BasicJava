7cac7f007e499a17fbb76d1b362dcdd3d6fd5c40
Harakire <halamala@mail.ru>
2013-09-30 20:33:39 +0400

    Добавление пакетов

---
+++ src/rt4/ahmetov/Main.java
@@ -0,0 +1,14 @@
+package rt4.ahmetov;
+
+import static java.lang.System.out;
+
+/**
+ * User: Maxim
+ * Date: 27.09.13
+ * Time: 0:11
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
