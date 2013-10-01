57a0c93f3535ce90937fec467e41cff6ce8d7365
din235 <din235@gmail.com>
2013-10-01 11:07:02 +0400

    Тестирование ввода/вывода

---
+++ src/rt4/chistov/InputOutputExample.java
@@ -0,0 +1,18 @@
+package rt4.chistov;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Din
+ * Date: 01.10.13
+ * Time: 10:28
+ * To change this template use File | Settings | File Templates.
+ */
+public class InputOutputExample {
+    public static void main(String[] args){
+       File file=new File("E/:File") ;
+    }
+}
